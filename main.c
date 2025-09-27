a0c6fa720f85450c59f3a3db849ce7cf15d41687
default-writer <funcelot@outlook.com>
2025-09-27 17:37:48 +0300

    updates

--- main.c
+++ main.c
@@ -14,30 +14,17 @@ int main(void) {
         strcpy_s(ptr, 20, data);
         printf("Allocated string: %s\n", ptr);
     }
-#if DEBUG
-    allocator_api->print_statistics(const_allocator_ptr);
-#endif    
+
     printf("\nRetaining the string...\n");
     char* str2 = allocator_api->retain(str);
     if (str2) {
         printf("Retained string: %s\n", str2);
     }
-#if DEBUG
-    allocator_api->print_statistics(const_allocator_ptr);
-#endif    
-    
     printf("\nReleasing one reference...\n");
-    
     allocator_api->release(str);
-#if DEBUG
-    allocator_api->print_statistics(const_allocator_ptr);
-#endif    
     
     printf("\nReleasing final reference...\n");
     allocator_api->release(str);
-#if DEBUG
-    allocator_api->print_statistics(const_allocator_ptr);
-#endif    
     
     printf("\nAllocating memory for an array...\n");
     const_sp_ptr_t numbers = allocator_api->alloc(const_allocator_ptr, sizeof(int) * 5);
@@ -52,18 +39,12 @@ int main(void) {
         }
         printf("\n");
     }
-#if DEBUG
-    allocator_api->print_statistics(const_allocator_ptr);
-#endif    
     
     printf("\nReleasing array...\n");
     allocator_api->release(numbers);
     allocator_api->gc(const_allocator_ptr);
     allocator_api->destroy(&const_allocator_ptr);
 
-#if DEBUG
-    allocator_api->print_statistics(const_allocator_ptr);
-#endif
     printf("\nDemo completed!\n");
     return 0;
 }
