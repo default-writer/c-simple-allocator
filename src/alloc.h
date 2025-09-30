88da4dd3cdaa75f549d2023ff742ceb399a8600c
default-writer <funcelot@outlook.com>
2025-09-30 05:06:08 +0300

    updates

--- src/alloc.h
+++ src/alloc.h
@@ -14,6 +14,27 @@ typedef struct allocator {
     int total_blocks;
 } allocator_t;
 
+#define BUCKET_COUNT 9
+extern const size_t bucket_sizes[BUCKET_COUNT];
+
+typedef struct free_list_node {
+    struct free_list_node* next;
+    struct free_list_node* prev;
+} free_list_node_t;
+
+typedef struct bucket {
+    free_list_node_t* free_list;
+    size_t block_size;
+} bucket_t;
+
+typedef struct bucket_allocator {
+    allocator_t base;
+    bucket_t buckets[BUCKET_COUNT];
+    void* memory_block;
+    size_t memory_offset;
+    size_t memory_size;
+} bucket_allocator_t;
+
 typedef struct sp {
     void* ptr;
     mem_block_t* block;
